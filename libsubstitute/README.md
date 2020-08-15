9f7ecdf2afbf390e1b7dc6b41c5fcb247b93b9ea
TheoryWrong <20625892+theorywrong@users.noreply.github.com>
2020-08-15 15:31:16 +0200

    Add libsubstitute

---
+++ libsubstitute/README.md
@@ -0,0 +1,100 @@
+# substitute
+libsusbtitute is the userland implementation for Mira Substitute system.
+
+Ported to OOSDK for PS4 by TheoryWrong on 8/15/2020
+
+## Notes 
+	- please use the oosdk_build.bat to build and install libsubstitute to oosdk.
+	- to link with libsubstitute, open your build.bat and add to libraries list -lsubstitute
+	
+## OOSDK Todo
+	- Create sample to compile for oosdk (Sample will be done soon)
+
+## Installation 
+The installation is done automatically by the build script
+
+## Usage
+Before using substitute, it should first be initialised using the `substitute_initialize()`
+function.
+Return nothing.
+
+```c
+void substitute_initialize();
+```
+
+For hook a SCE Function, you need to use
+```c
+struct substitute_hook* substitute_hook(const char* module_name, const char* name, void* hook_function, int flags);
+```
+The default module_name is SUBSTITUTE_MAIN_MODULE (it's simply the eboot/main program), but you can hook function from another module
+
+For flags, 2 exist :
+- SUBSTITUTE_IAT_NAME : Use the original name (like `sceUserServiceGetUserName`)
+- SUBSTITUTE_IAT_NIDS : Use the nids as name (like `1xxcMiGu2fo`)
+
+Return NULL if a error occurs.
+
+Example :
+```c
+struct substitute_hook* username_hook;
+
+void sceUserServiceGetUserName_hook(uint64_t userId, char *userName, const size_t size) {
+	SUBSTITUTE_WAIT(username_hook); // Wait for the hook object
+
+	SUBSTITUTE_CONTINUE(void, username_hook, (uint64_t, char*, const size_t), userId, userName, size); // Call the original function, with original argument
+
+	printf("The username is: %s", userName); // Show the original username
+	strncpy(userName, "mycustomname", 12); // Inject a custom username instead
+}
+
+...
+
+int module_start() {
+	substitute_initialize();
+
+	username_hook = substitute_hook(SUBSTITUTE_MAIN_MODULE, "sceUserServiceGetUserName", sceUserServiceGetUserName_hook, SUBSTITUTE_IAT_NAME);
+	if (!username_hook) {
+		printf("Unable to hook username function.\n");
+	}
+
+	return 0;
+}
+
+```
+
+Some explaination of macro here:
+`SUBSTITUTE_WAIT(hook)` is a macro for check when the hook object is available
+`SUBSTITUTE_CONTINUE(type, hook, argl, ...)` is a macro for call the original function. If you don't need it, don't call him.
+
+When you are done with the hook, you can delete it by using `substitute_unhook`.
+Return 0 when everything is ok.
+
+```c
+struct substitute_hook* username_hook;
+
+...
+
+int module_stop() {
+	if (substitute_unhook(username_hook) > 0) {
+		printf("Unable to unhook the username hook\n");
+	}
+
+	return 0;
+}
+```
+
+You can also simply disable/enable a hook for a moment by `substitute_disable` and `substitute_enable`
+```c
+int substitute_disable(struct substitute_hook* hook);
+int substitute_enable(struct substitute_hook* hook);
+```
+
+## Note
+
+You need to got the last version of Mira.
+For install the plugin inside the console, use the FTP or any other File Manager.
+The folder structure need to respect this case (For example with game CUSA00001 "The Playroom")
+
+`/data/mira/substitute/CUSA00001/ThePlugin.sprx`
+
+The plugin is launched automatically during the load of the game and before this entrypoint.
---
+++ libsubstitute/src/substitute.c
@@ -0,0 +1,142 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#include <orbis/libkernel.h>
+
+#include "substitute.h"
+
+int(*_ioctl)(int, unsigned long, ...) = NULL;
+
+void substitute_initialize() {
+  uint32_t kernel_module = sceKernelLoadStartModule("libkernel.sprx", 0, NULL, 0, NULL, NULL);
+  sceKernelDlsym(kernel_module, "ioctl", (void**)&_ioctl);
+
+  if (!_ioctl) {
+    printf("libsubstitute -> unable to get ioctl function.\n");
+  }
+}
+
+struct substitute_hook* substitute_hook(const char* module_name, const char* name, void* hook_function, int flags) {
+  if (!_ioctl) {
+    printf("libsubstitute -> unable to get ioctl function.\n");
+  }
+
+  if (strlen(module_name) > SUBSTITUTE_MAX_NAME) {
+    printf("libsubstitute -> mira_hook_iat(module_name): SUBSTITUTE_MAX_NAME reached.\n");
+    return NULL;
+  }
+
+  if (strlen(name) > SUBSTITUTE_MAX_NAME) {
+    printf("libsubstitute -> mira_hook_iat(name): SUBSTITUTE_MAX_NAME reached.\n");
+    return NULL;
+  }
+
+  // Setup chain & information system
+  struct substitute_hook* hook = (struct substitute_hook*)malloc(sizeof(substitute_hook));
+
+  if (!hook) {
+    printf("libsubstitute -> mira_hook_iat: unable to malloc mira_hook structure !\n");
+    return NULL;
+  }
+
+  memset(hook, 0, sizeof(struct substitute_hook));
+
+  // Call mira hook system
+  int mira_device = open("/dev/mira", O_RDWR);
+  if (mira_device < 0) {
+    printf("libsubstitute -> mira_hook_iat: can't open mira device (%d).\n", mira_device);
+    return NULL;
+  }
+
+  // Setup parameter
+  struct substitute_hook_iat param;
+  param.hook_id = -1; // Pre-set for prevent to get 0 if something append
+
+  param.hook_function = hook_function;
+  param.flags = flags;
+  param.chain = hook;
+  strncpy(param.name, name, SUBSTITUTE_MAX_NAME);
+  strncpy(param.module_name, module_name, SUBSTITUTE_MAX_NAME);
+
+  // Do ioctl
+  int ret = _ioctl(mira_device, MIRA_IOCTL_IAT_HOOK, &param);
+
+  if (ret != 0) {
+    printf("libsubstitute -> mira_hook_iat: ioctl error (%d).\n", ret);
+    free(hook);
+    return NULL;
+  }
+
+  close(mira_device);
+
+  // Check returned data
+  if (param.hook_id < 0) {
+    printf("libsubstitute -> mira_hook_iat: data returned is invalid !\n");
+    free(hook);
+    return NULL;
+  }
+
+  printf("libsubstitute -> new hook iat !\nhook_id: %i\nchain_structure: %p\n", param.hook_id, param.chain);
+  return hook;
+}
+
+int substitute_statehook(struct substitute_hook* hook, int state) {
+  if (!_ioctl) {
+    printf("libsubstitute -> unable to get ioctl function.\n");
+    return 1;
+  }
+
+  if (!hook) {
+    printf("libsubstitute -> mira_hook_iat: invalid parameter.\n");
+    return 1;
+  }
+
+  // Call mira hook system
+  int mira_device = open("/dev/mira", O_RDWR);
+  if (mira_device < 0) {
+    printf("libsubstitute -> mira_hook_iat: can't open mira device (%d).\n", mira_device);
+    return 1;
+  }
+
+  // Setup parameter
+  struct substitute_state_hook param;
+  param.hook_id = hook->hook_id;
+  param.state = state;
+  param.chain = hook;
+
+  // Do ioctl
+  int ret = _ioctl(mira_device, MIRA_IOCTL_STATE_HOOK, &param);
+  close(mira_device);
+
+  if (ret < 0) {
+    printf("libsubstitute -> mira_state_hook: ioctl error (%d).\n", ret);
+    return 1;
+  }
+
+  // Close device
+  close(mira_device);
+
+  // Check returned data
+  if (param.result < 0) {
+    printf("libsubstitute -> mira_hook_iat: data returned is invalid (ret: %d) !\n", param.result);
+    return 1;
+  }
+
+  return 0;
+}
+
+int substitute_disable(struct substitute_hook* hook) {
+  return substitute_statehook(hook, SUBSTITUTE_STATE_DISABLE);
+}
+
+int substitute_enable(struct substitute_hook* hook) {
+  return substitute_statehook(hook, SUBSTITUTE_STATE_ENABLE);
+}
+
+int substitute_unhook(struct substitute_hook* hook) {
+  return substitute_statehook(hook, SUBSTITUTE_STATE_UNHOOK);
+}
